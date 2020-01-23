4c2c7f77eb410faecc0090113b6fc8300aab1e6d
anthon78 <economicsstudent2000@gmail.com>
2020-01-22 18:42:37 -0600

    set up client/server/webpack/package.json boilerplate

---
+++ src/components/MapSite.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class MapSite extends React.Component {
+
+  render() {
+    return (
+      <div>
+        working App!!
+      </div>
+    )
+  }
+}
+
+export default MapSite;
+
+
---
+++ src/server.js
@@ -0,0 +1,6 @@
+const express = require("express")
+let app = express()
+
+
+app.use(express.static(__dirname + './../public'));
+app.listen(8080, () => console.log("App listening on port 8080!"));
